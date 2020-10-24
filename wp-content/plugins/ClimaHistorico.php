0132e11bb5ec06bfd16cd84d498bb73ee2ffeb67
Diego Vargas <diego@soho.cl>
2020-10-24 11:45:26 -0500

    add plugin

---
+++ wp-content/plugins/ClimaHistorico.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Consumir API Clima
+Plugin URI: https://openweathermap.org/history
+Description: Plugin para prueba gradinweb
+Version: 1.0
+Author: Diego Vargas
+Author URI: https://diego.gridsoft.co
+License: GPL2
+*/
+
